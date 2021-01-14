fe15e96b528a6168b6ba874a979725438d06f3eb
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-01-13 19:43:46 -0800

    feat: Add navlist creator function

---
+++ js/app.js
@@ -0,0 +1,66 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const navList = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+const navBuild = () => {
+    for (section of sections) {
+        const li = document.createElement('li');
+        li.innerHTML = `<a href="#${section.getAttribute('id')}" class="menu__link">${section.getAttribute('data-nav')}</a>`;
+        navList.append(li);
+    }
+}
+
+navBuild();
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
